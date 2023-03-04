9a74e65e700d34bbbbb33edd4e56f02b73dbd3e4
António Cortez <aamcortez@gmail.com>
2023-03-04 14:48:05 +0000

    added timestamps to wound

--- models/Wound.model.js
+++ models/Wound.model.js
@@ -1,11 +1,14 @@
 const { Schema, model } = require("mongoose");
 
-const woundSchema = new Schema({
-    pictureUrl: String,
-    description: String,
-    treatment: String,
-    patient: { type: Schema.Types.ObjectId, ref: "Patient" },
-});
+const woundSchema = new Schema(
+   {
+      pictureUrl: String,
+      description: String,
+      treatment: String,
+      patient: { type: Schema.Types.ObjectId, ref: "Patient" },
+   },
+   { timestamp: true }
+);
 
 const Wound = model("Wound", woundSchema);
 
